9e71eb74b7c0e754579fe59862cec39cc4f80df1
fares002 <fares@example.com>
2023-11-29 18:45:40 -0800

    if && elif

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('q') and letter != ord('e'):
-        print('{:c}'.format(letter), end='')
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['q', 'e']:
+        print(f"{chr(char)}", end='')
+
