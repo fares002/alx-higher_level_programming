e50f01c4b8c2bc511411ca61d12e9f58a4a34338
fares002 <fares@example.com>
2023-11-29 18:51:06 -0800

    if && elif

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    print('{:02d}'.format(i), end=', ')
-print(99)
+for i in range(0, 100):
+    if i == 99:
+        print(i)
+    else:
+        print("{:0>2d}".format(i), end=", ")
