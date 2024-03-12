0ee01a33b3b6ab9e60134e54fb6546b3266b7162
fares002 <fares@example.com>
2024-03-11 19:18:32 -0700

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const Thefirstline = 'C is fun';
-const Thesecondline = 'Python i scool';
-const Thethirdline = 'JavaScript is amazing';
-console.log(Thefirstline);
-console.log(Thesecondline);
-console.log(Thethirdline);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
