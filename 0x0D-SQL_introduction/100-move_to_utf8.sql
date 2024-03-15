e3182329ad69e4ec06e4b7b66ae00300be25ba52
fares002 <fares@example.com>
2024-03-14 21:24:56 -0700

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,17 @@
 -- Convert a database to utf-8
-ALTER TABLE `hbtn_0c_0`.`first_table` MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `hbtn_0c_0`.`first_table` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert database to utf8mb4
+ALTER DATABASE `hbtn_0c_0`
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+-- Convert table to utf8mb4
+ALTER TABLE `hbtn_0c_0`.`first_table`
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Convert column to utf8mb4
+ALTER TABLE `hbtn_0c_0`.`first_table`
+CHANGE `name` `name` VARCHAR(100)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
