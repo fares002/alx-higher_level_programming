43c5a8774f1931518c8415904e62f817d1d9c457
fares002 <fares@example.com>
2024-03-17 18:00:51 -0700

    mysql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
- lists all states from the database hbtn_0e_0_usa
+"""lists all states from the database hbtn_0e_0_usa
+ Usage: ./0-select_states.py <mysql username>
+                            <mysql password>
+                            <database name >
 """
 
 import sys
 import MySQLdb
 
-if __name__ == "__main__"
-con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ") # HERE I have to know SQL to grab all states in my database
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
